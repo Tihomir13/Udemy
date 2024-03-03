54bcf6f9e4a2bc880c82581427d42bf85db53800
Tihomir13 <tihomir13n2002@abv.bg>
2024-03-03 14:19:36 +0200

    updating 255 video

--- JavaScriptCourse/complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ JavaScriptCourse/complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -3,6 +3,30 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, className = '') {
+    const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>
+    `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    // countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+    countriesContainer.insertAdjacentText('beforeend', msg);
+    // countriesContainer.style.opacity = 1;
+};
+
 ///////////////////////////////////////
 // const getCountryData = function (country) {
 
@@ -36,69 +60,103 @@ const countriesContainer = document.querySelector('.countries');
 // getCountryData('portugal');
 // getCountryData('usa');
 
-const renderCountry = function (data, className = '') {
+// const getCountryAndNeighbour = function (country) {
 
-    const html = ` 
-        <article class="country ${className}">
-        <img class="country__img" src="${data.flag}" />
-        <div class="country__data">
-        <h3 class="country__name">${data.name}</h3>
-        <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
-        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-        </div>
-        </article>`;
+//     // AJAX call country 1
+//     const request = new XMLHttpRequest();
+//     request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//     request.send();
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+//     request.addEventListener('load', function () {
+//         console.log(this.responseText);
 
-}
-const getCountryAndNeighbour = function (country) {
+//         const [data] = JSON.parse(this.responseText);
+//         console.log(data);
 
-    // AJAX call country 1
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.com/v2/name/${country}`);
-    request.send();
+//         //Render country 1
+//         renderCountry(data);
 
-    request.addEventListener('load', function () {
-        console.log(this.responseText);
+//         //Get neighbour country (2)
+//         const neighbour = data.borders?.[0];
 
-        const [data] = JSON.parse(this.responseText);
-        console.log(data);
+//         if (!neighbour) return;
 
-        //Render country 1
-        renderCountry(data);
+//         // AJAX call country 2
+//         const request2 = new XMLHttpRequest();
+//         request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+//         request2.send();
 
-        //Get neighbour country (2)
-        const neighbour = data.borders?.[0];
+//         request2.addEventListener('load', function () {
+//             const data2 = JSON.parse(this.responseText);
+//             console.log(data2);
 
-        if (!neighbour) return;
+//             renderCountry(data2, 'neighbour');
+//         });
+//     });
+// };
 
-        // AJAX call country 2
-        const request2 = new XMLHttpRequest();
-        request2.open('GET', `https://restcountries.com/v2/alpha/${country}`);
-        request2.send();
+// setTimeout(() => {
+//     console.log(`1 seconds passed`);
+//     setTimeout(() => {
+//         console.log(`2 seconds passed`);
+//         setTimeout(() => {
+//             console.log(`3 seconds passed`);
+//             setTimeout(() => {
+//                 console.log(`4 seconds passed`);
+//             }, 1000);
+//         }, 1000);
+//     }, 1000);
+// }, 1000);
+// getCountryAndNeighbour('portugal');
 
-        request2.addEventListener('load', function () {
-            const data2 = JSON.parse(this.responseText);
-            console.log(data2);
+//     const request = new XMLHttpRequest();
+//     request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//     request.send();
 
-            renderCountry(data2, 'neighbour');
-        });
-    });
+// const request = fetch('https://restcountries.com/v2/name/portugal');
+// console.log(request);
+
+// const getCountryData = function (country) {
+//     fetch(`https://restcountries.com/v2/name/${country}`)
+
+//         .then(function (response) {
+//             console.log(response);
+//             return response.json();
+//         })
+
+//         .then(function (data) {
+//             console.log(data);
+//             renderCountry(data[0]);
+//         });
+// };
+
+const getCountryData = function (country) {
+
+    // Country 1
+    fetch(`https://restcountries.com/v2/name/${country}`)
+        .then(response => response.json())
+        .then(data => {
+            renderCountry(data[0]);
+            const neighbour = data[0].borders?.[0];
+
+            if (!neighbour) return;
+
+            // Country 2
+            return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+        })
+        .then(response => response.json())
+        .then(data => renderCountry(data, 'neighbour'))
+        .catch(err => {
+            console.error(`${err} 💥💥💥`);
+            renderError(`Something went wrong 💥💥 ${err
+                .message}. Try again!`);
+        })
+        .finally(() => {
+            countriesContainer.style.opacity = 1;
+        })
 };
+btn.addEventListener('click', function () {
+    getCountryData('portugal');
+});
 
-setTimeout(() => {
-    console.log(`1 seconds passed`);
-    setTimeout(() => {
-        console.log(`2 seconds passed`);
-        setTimeout(() => {
-            console.log(`3 seconds passed`);
-            setTimeout(() => {
-                console.log(`4 seconds passed`);
-            }, 1000);
-        }, 1000);
-    }, 1000);
-}, 1000);
-getCountryAndNeighbour('portugal');
+getCountryData('dsfddsfsdf');
