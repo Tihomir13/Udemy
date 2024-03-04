519d2b6954f58cec89d2fec87849e3f7f09d0d4d
Tihomir13 <tihomir13n2002@abv.bg>
2024-03-04 22:09:09 +0200

    update

--- JavaScriptCourse/complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ JavaScriptCourse/complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -236,3 +236,45 @@ console.log(`Test Start`);
 setTimeout(() => console.log(`0 sec timer`), 0);
 Promise.resolve(`Resolved promis 1`).then(res => console.log(res));
 console.log(`Test end`);
+
+//////////////////////////////
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+    console.log(`Lotter draw is happening 🔮`);
+    setTimeout(function () {
+        if (Math.random() >= 0.5) {
+            resolve(`You WIN 💰`);
+        }
+        else {
+            reject(new Error(`You Lost your money 💩`));
+        }
+
+    }, 2000)
+});
+
+lotteryPromise
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+
+const wait = function (seconds) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, seconds * 1000)
+    });
+};
+
+wait(1)
+    .then(() => {
+        console.log(`1 second passed`);
+        return wait(1);
+    })
+    .then(() => {
+        console.log(`2 second passed`);
+        return wait(1);
+    })
+    .then(() => {
+        console.log(`3 second passed`);
+        return wait(1);
+    })
+
+Promise.resolve(`abc`).then(x => console.log(x));
+Promise.reject(new Error(`Problem!`)).catch(x => console.error(x));
