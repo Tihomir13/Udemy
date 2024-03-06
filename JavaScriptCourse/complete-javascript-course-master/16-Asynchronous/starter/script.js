72b98a8c8181b0ab09de07e5746d5e5089ee1799
Tihomir13 <tihomir13n2002@abv.bg>
2024-03-07 00:36:46 +0200

    update ajax script

--- JavaScriptCourse/complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ JavaScriptCourse/complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -420,3 +420,68 @@ const get3Countries = async function (c1, c2, c3) {
     }
 }
 get3Countries(`portugal`, `canada`, `tanzania`);
+
+// Promise.race
+
+(async function () {
+    const res = await Promise.race([
+        getJSON(`https://restcountries.com/v2/name/italy`),
+        getJSON(`https://restcountries.com/v2/name/egypt`),
+        getJSON(`https://restcountries.com/v2/name/mexico`)])
+})
+
+//Chalange 3
+
+const wait = function (seconds) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, seconds * 1000)
+    });
+}
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+    return new Promise(function (resolve, reject) {
+        const img = document.createElement(`img`);
+        img.src = imgPath;
+
+        img.addEventListener('load', function () {
+            imgContainer.append(img);
+            resolve(img);
+        });
+
+        img.addEventListener(`error`, function () {
+            reject(new Error(`cannot found`));
+        })
+    });
+}
+
+let currentImg;
+
+createImage(`img\img-1.jpg`)
+    .then(img => {
+        currentImg = img;
+        console.log(`Image 1 loaded`);
+        return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = `none`;
+        return new Promise('img/img-2.jpg');
+    })
+    .then(img => {
+        currentImg = img;
+        console.log(`Image 2 loaded`);
+        return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = `none`;
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+const loadNPause = async function () {
+    let img1 = await (`img\img-1.jpg`)
+    console.log(`Image 1 loaded`);
+    return wait(2);
+}
