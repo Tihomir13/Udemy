99de1cfeaca0211f50687c99e833e87d846f33ef
Tihomir13 <tihomir13n2002@abv.bg>
2024-03-10 15:07:59 +0200

    update

--- JavaScriptCourse/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ JavaScriptCourse/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -2,5 +2,75 @@
 import { addToCart, totalPrice as price, totalQuantity } from "./shoppingCart.js";
 console.log(`Importing module`);
 
+/*
 addToCart(`bread`, 5);
 console.log();
+
+// const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+// const data = await res.json();
+
+// console.log(data);
+
+const getLastPost = async function () {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    const data = await res.json();
+    console.log(data);
+    
+    return {
+        title: data.at(-1).title,
+        text: data.at(-1).body
+    };
+}
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not very clean
+lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+
+const ShoppingCart2 = (function () {
+    const cart = [];
+    const shippingCost = 10;
+    const totalPrice = 237;
+    const totalQuantity = 23;
+    
+    const addToCart = function (product, quantity) {
+        cart.push({ product, quantity });
+        console.log(`${quantity} ${product} added to cart`);
+    };
+    
+    const orderStock = function (product, quantity) {
+        cart.push({ product, quantity });
+        console.log(`${quantity} ${product} added to cart`);
+    };
+    
+    return {
+        addToCart,
+        cart,
+        totalPrice,
+        totalQuantity,
+    };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+*/
+
+import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
+
+const state = {
+    cart: [
+        { product: 'bread', quantity: 5 },
+        { product: 'pizza', quantity: 5 },
+    ],
+    user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+state.user.loggedIn = false;
+console.log(stateClone);
+
+const stateDeepClone = cloneDeep(state);
+console.log(stateDeepClone);
