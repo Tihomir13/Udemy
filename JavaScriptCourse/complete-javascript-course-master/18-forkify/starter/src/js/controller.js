98adb5891f6cf85c083fafbc9b90a1747aabba2c
Tihomir13 <tihomir13n2002@abv.bg>
2024-03-16 14:08:39 +0200

    update the last app

--- JavaScriptCourse/complete-javascript-course-master/18-forkify/starter/src/js/controller.js
+++ JavaScriptCourse/complete-javascript-course-master/18-forkify/starter/src/js/controller.js
@@ -1,3 +1,9 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -9,5 +15,32 @@ const timeout = function (s) {
 };
 
 // https://forkify-api.herokuapp.com/v2
+//5ed6604591c37cdc054bcc40
+//      "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
 
-///////////////////////////////////////
+/////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1) Loading recipe
+    await model.loadRecipe(id);
+
+    // 2) Rendering recipe
+    recipeView.render(model.state.recipe)
+  } catch (err) {
+    alert(err);
+  }
+}
+
+controlRecipes();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipes))
+
+// window.addEventListener('hashchange', controlRecipes);
+// window.addEventListener('load', controlRecipes);
