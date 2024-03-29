86ec3d1cb1811419be5f175ad6fade71441a7e44
Тихомир <64681422+Tihomir13@users.noreply.github.com>
2024-03-29 09:31:09 +0200

    Add files via upload

---
+++ The Complete 2024 Web Development Bootcamp/2.3 NPM/index.js
@@ -0,0 +1,8 @@
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+import superhero from "superheroes";
+const heroName = superhero.random();
+console.log(`Im ${heroName}`);
