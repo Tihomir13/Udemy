96a4e40af4e401f38b636cccad429fa9a215e63b
Tihomir13 <tihomir13n2002@abv.bg>
2024-04-01 01:37:24 +0300

    finishing 5.6 and the section

--- 2024 Web Development Bootcamp/BackEnd/5.6 Secrets Project/index.js
+++ 2024 Web Development Bootcamp/BackEnd/5.6 Secrets Project/index.js
@@ -1,3 +1,45 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+})
+
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get('https://secrets-api.appbrewery.com/random');
+        res.render("index.ejs", { secret: response.data.secret, user: response.data.username });
+    }
+    catch (error) {
+        res.render("index.ejs", { secret: error.response.message });
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // HINTS:
 // 1. Import express and axios
 
@@ -11,3 +53,4 @@
 // secret and the username of the secret.
 
 // 6. Listen on your predefined port and start the server.
+
