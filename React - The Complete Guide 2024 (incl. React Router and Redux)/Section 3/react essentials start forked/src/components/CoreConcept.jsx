8a54285a7aabf9b634693817d56b93f6f49afd73
Tihomir13 <tihomir13n2002@abv.bg>
2024-04-14 23:44:16 +0300

    updating section 3 (essentials)

---
+++ React - The Complete Guide 2024 (incl. React Router and Redux)/Section 3/react essentials start forked/src/components/CoreConcept.jsx
@@ -0,0 +1,8 @@
+export default function CoreConcept({img,title,description} = props) {
+    return (
+    <li>
+      <img src = {img} alt={title}/>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>)
+  }
