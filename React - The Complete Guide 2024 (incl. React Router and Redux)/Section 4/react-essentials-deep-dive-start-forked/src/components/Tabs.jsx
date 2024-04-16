cf754cabf39936f14b4a33dd53bac0f9a5b3b193
Tihomir13 <tihomir13n2002@abv.bg>
2024-04-16 14:16:22 +0300

    Finishing Section 4 first project

---
+++ React - The Complete Guide 2024 (incl. React Router and Redux)/Section 4/react-essentials-deep-dive-start-forked/src/components/Tabs.jsx
@@ -0,0 +1,10 @@
+import TabButton from "./TabButton";
+
+export default function Tabs ({buttons, children, ButtonsContainer = "menu"}) {
+    return <>
+        <ButtonsContainer>
+            {buttons}
+        </ButtonsContainer>
+        {children}
+    </>
+}
