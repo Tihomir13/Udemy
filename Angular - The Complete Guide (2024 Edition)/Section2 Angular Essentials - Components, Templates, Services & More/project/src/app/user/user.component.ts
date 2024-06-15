288be2fcf896092e498d1330bb61be338280b893
Tihomir13 <tihomir13n2002@abv.bg>
2024-06-15 20:51:59 +0300

    update Section 2

--- Angular - The Complete Guide (2024 Edition)/Section2 Angular Essentials - Components, Templates, Services & More/project/src/app/user/user.component.ts
+++ Angular - The Complete Guide (2024 Edition)/Section2 Angular Essentials - Components, Templates, Services & More/project/src/app/user/user.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -10,9 +9,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
   get imagePath() {
-    return '../../assets/users/' + this.selectedUser.avatar;
+    return '../../assets/users/' + this.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.id);
   }
 }
