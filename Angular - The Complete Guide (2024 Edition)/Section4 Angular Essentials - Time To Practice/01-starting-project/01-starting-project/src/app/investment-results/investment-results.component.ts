c8e169f88d5151a35e0a9ada77ce7675bb1c7108
Tihomir13 <tihomir13n2002@abv.bg>
2024-06-19 17:20:12 +0300

    finishing Section 4

---
+++ Angular - The Complete Guide (2024 Edition)/Section4 Angular Essentials - Time To Practice/01-starting-project/01-starting-project/src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,18 @@
+import { Component, inject } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  imports: [CurrencyPipe],
+  standalone: true,
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css',
+})
+export class InvestmentResultsComponent {
+  constructor(private investmentService: InvestmentService) {}
+
+  get results() {
+    return this.investmentService.resultsData;
+  }
+}
