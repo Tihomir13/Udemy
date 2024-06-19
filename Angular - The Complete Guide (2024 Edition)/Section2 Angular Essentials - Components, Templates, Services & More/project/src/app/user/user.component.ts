71a26f9cbfd1cec8ef1b217ba87d9261a0ae5007
Tihomir13 <tihomir13n2002@abv.bg>
2024-06-19 08:20:12 +0300

    finishing Section 2

--- Angular - The Complete Guide (2024 Edition)/Section2 Angular Essentials - Components, Templates, Services & More/project/src/app/user/user.component.ts
+++ Angular - The Complete Guide (2024 Edition)/Section2 Angular Essentials - Components, Templates, Services & More/project/src/app/user/user.component.ts
@@ -1,24 +1,24 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 
 @Component({
-  selector: 'app-user',
-  standalone: true,
-  imports: [],
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css',
+    selector: 'app-user',
+    standalone: true,
+    templateUrl: './user.component.html',
+    styleUrl: './user.component.css',
+    imports: [CardComponent]
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return '../../assets/users/' + this.avatar;
+    return '../../assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
