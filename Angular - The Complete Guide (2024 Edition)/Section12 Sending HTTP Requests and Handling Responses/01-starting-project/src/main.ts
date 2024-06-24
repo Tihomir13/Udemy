205b78824b08d1c8b0fd17fa0755982efa9c5b92
Tihomir13 <tihomir13n2002@abv.bg>
2024-06-24 12:41:33 +0300

    finishing Section 12

---
+++ Angular - The Complete Guide (2024 Edition)/Section12 Sending HTTP Requests and Handling Responses/01-starting-project/src/main.ts
@@ -0,0 +1,24 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { AppComponent } from './app/app.component';
+import {
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+
+function loggingInterceptor(
+  request: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) {
+  // const req = request.clone({
+  //   headers: request.headers.set('X-DEBUG', 'TESTING'),
+  // });
+  console.log('[Outgoing Request]');
+  return next(request);
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [provideHttpClient(withInterceptors([loggingInterceptor]))],
+}).catch((err) => console.error(err));
