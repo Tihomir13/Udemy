fbfe5e4c30618f4cb9da7619257ffa2e1c2fb302
Tihomir13 <tihomir13n2002@abv.bg>
2024-10-20 22:58:23 +0300

    starting Section 3

---
+++ Ethereum and Solidity The Complete Developer's Guide/Section 3/lottery/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { Web3 } = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'cereal combine congress truly hospital sustain lab peace parrot one view square',
+  'https://sepolia.infura.io/v3/75d62a1e49894f8883a428eb6fd64032'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log(interface);
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
