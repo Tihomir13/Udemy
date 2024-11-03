610a4ca0544693ce1c7c9b58056e79368e73d80d
Tihomir13 <tihomir13n2002@abv.bg>
2024-11-03 18:34:55 +0200

    finishing Section 4

---
+++ Ethereum and Solidity The Complete Developer's Guide/Section 4/lottery-react-boilerplate/lottery-react/src/lottery.js
@@ -0,0 +1,60 @@
+import Web3 from 'web3';
+
+const address = '0x0c5631b428bF9dD2700e5DA648E803857f62b938';
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: 'manager',
+    outputs: [{ name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'pickWinner',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'getPlayers',
+    outputs: [{ name: '', type: 'address[]' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'enter',
+    outputs: [],
+    payable: true,
+    stateMutability: 'payable',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [{ name: '', type: 'uint256' }],
+    name: 'players',
+    outputs: [{ name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+];
+
+const web3 = new Web3(window.ethereum);
+
+export default new web3.eth.Contract(abi, address);
