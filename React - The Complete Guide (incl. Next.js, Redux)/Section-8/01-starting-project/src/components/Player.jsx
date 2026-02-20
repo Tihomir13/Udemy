1819dfefd15c193da89903e44588cc3ea6a1901f
Tihomir13 <tihomir13n2002@abv.bg>
2026-02-20 16:19:22 +0200

    update Section 8 | React course

--- React - The Complete Guide (incl. Next.js, Redux)/Section-8/01-starting-project/src/components/Player.jsx
+++ React - The Complete Guide (incl. Next.js, Redux)/Section-8/01-starting-project/src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerNameRef = useRef();
+
+  const [savedPlayerName, setSavedPlayerName] = useState("unknown");
+
+  function savePlayerName() {
+    setSavedPlayerName(playerNameRef.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {savedPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          ref={playerNameRef}
+          type="text"
+        />
+        <button onClick={savePlayerName}>Set Name</button>
       </p>
     </section>
   );
