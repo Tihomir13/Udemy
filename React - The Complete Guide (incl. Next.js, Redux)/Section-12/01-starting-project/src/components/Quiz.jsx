27b0fb007decb51a26cba0c98eb79aa85e007302
Tihomir13 <tihomir13n2002@abv.bg>
2026-02-27 14:09:31 +0200

    Finishing Section 12 | React course

---
+++ React - The Complete Guide (incl. Next.js, Redux)/Section-12/01-starting-project/src/components/Quiz.jsx
@@ -0,0 +1,34 @@
+import { useCallback, useState } from "react";
+import QUESTIONS from "../questions";
+import Question from "./Question";
+import Summary from "./Summary";
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+  const isQuizComplete = activeQuestionIndex === QUESTIONS.length;
+
+  const handleSelectAnswer = useCallback((selectedAnswer) => {
+    setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
+  }, []);
+
+  const handleSkipAnswer = useCallback(() => {
+    handleSelectAnswer(null);
+  }, [handleSelectAnswer]);
+
+  if (isQuizComplete) {
+    return <Summary userAnswers={userAnswers} />;
+  }
+
+  return (
+    <div id="quiz">
+      <Question
+        key={activeQuestionIndex}
+        questionIndex={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
+    </div>
+  );
+}
