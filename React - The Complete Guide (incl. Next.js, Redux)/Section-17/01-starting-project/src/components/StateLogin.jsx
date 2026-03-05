0a4216bec8cea7cf30d5c6f01a282b87de63ef6f
Tihomir13 <tihomir13n2002@abv.bg>
2026-03-05 08:31:34 +0200

    Finishing Section 17 | React course

---
+++ React - The Complete Guide (incl. Next.js, Redux)/Section-17/01-starting-project/src/components/StateLogin.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { use } from "react";
+
+export default function StateLogin() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const emailIsInvalid =
+    enteredValues.email != "" && !enteredValues.email.includes("@");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log("Logging in...");
+  }
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+
+    console.log(event.target);
+
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange(event)}
+            value={enteredValues.email}
+          />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) => handleInputChange(event)}
+            value={enteredValues.password}
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
